505f54f82e86d8ac40af1db763ed9b459054b677
Hugo <blackfoxmmm@gmail.com>
2021-09-06 14:19:33 +0500

    language

---
+++ src/Localization/Content.js
@@ -0,0 +1,73 @@
+const content = {
+    en:{
+        home: 'Home',
+        sharp: 'Explore',
+        colocol:'Notification',
+        message:'Message',
+        zakladka:'Bookmarks',
+        list:'List',
+        profile:'Profile',
+        more:'More',
+        inputone:'What’s happening',
+       
+
+        inputtwo:'Search Twitter',
+        trendsheader:'Trends for you',
+
+        trending:'Trending in Germany',
+        trends_revolution:'Revolution',
+        revolution_tweet:'50.4K Tweets',
+
+        showbtn:'Show more',
+        mightheader:'You might like',
+       
+        blackbtn:'Follow',
+        tweetbtn:'Tweet',
+
+        footer_terms:'Terms of Service',
+        footer_privacy:'Privacy Policy',
+        footer_cookie:'Cookie Policy',
+        footer_imprint:'Imprint',
+        footer_info:'Ads Info',
+        footer_more:'More ···',
+        footer_twitter:'© 2021 Twitter, Inc.',
+
+
+    },
+
+
+    ru:{
+        home: 'Дом',
+        sharp: 'Исследовать',
+        colocol:'Уведомление',
+        message:'Сообщение',
+        zakladka:'Закладки',
+        list:'Список',
+        profile:'Профиль',
+        more:'Более',
+        inputone:'Что творится',
+    
+        inputtwo:'Искать Twitter',
+        trendsheader:'Тенденции для вас',
+
+        trending:'В тренде в Германии',
+        trends_revolution:'Революция',
+        revolution_tweet:'50.4K Твиты',
+
+        showbtn:'Больше',
+        mightheader:'Вы могли бы',
+        
+        blackbtn:'Подписаться',
+        tweetbtn:'Твитнуть',
+
+        footer_terms:'условия обслуживания',
+        footer_privacy:'Privacy Policy',
+        footer_cookie:'Политика конфиденциальности',
+        footer_imprint:'Отпечаток',
+        footer_info:'Информация о рекламе',
+        footer_more:'Более ···',
+        footer_twitter:'© 2021 Twitter, Inc.',
+
+
+    }
+}
